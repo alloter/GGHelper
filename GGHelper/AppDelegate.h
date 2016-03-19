430cc74e4ffb0752d0ee659649f8c6e35f22e983
失落王城 <1296696830@qq.com>
2016-03-19 08:39:13 +0800

    Initial Commit

---
+++ GGHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGHelper
+//
+//  Created by alloter on 16/3/19.
+//  Copyright © 2016年 alloter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGHelper
+//
+//  Created by alloter on 16/3/19.
+//  Copyright © 2016年 alloter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
